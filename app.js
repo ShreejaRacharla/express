88bbd49c243ec3a947b2f25d8a4120339164f9f9
unknown <shreeja.racharlas@gmail.com>
2026-02-02 13:24:36 +0530

    first

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello from Express App!");
+});
+
+app.get("/about", (req, res) => {
+  res.send("This is About Page");
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
