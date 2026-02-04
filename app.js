fedb12cbaeced23db66fca5d41773c9e9dcfe9de
unknown <shreeja.racharlas@gmail.com>
2026-02-04 22:18:45 +0530

    testing flow

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello from Express App!");
+  res.send("Hello from Express App, hi!");
 });
 
 app.get("/about", (req, res) => {
-  res.send("This is About Page");
+  res.send("Hi! This is About Page");
 });
 
-app.listen(3000, () => {
+app.listen(3001, () => {
   console.log("Server running on port 3001");
 });
